6242de86d771bad863bfa58009e63b94a5d5e8fd
iWillBanU <wrscr.surpertop@gmail.com>
2020-12-01 11:46:37 -0500

    Create index.js

---
+++ day3/puzzle1/index.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+const input = fs.readFileSync("./input.txt", {encoding: "utf-8"});
+
+const inputArray = input.split("");
+const memory = {"0,0": 1};
+
+let x = 0;
+let y = 0;
+for (let i = 0; i < inputArray.length; i++) {
+  switch (inputArray[i]) {
+    case "^":
+      y++;
+      break;
+    case "v":
+      y--;
+      break;
+    case ">":
+      x++;
+      break;
+    case "<":
+      x--;
+      break;
+  }
+  if (!memory[x + "," + y]) memory[x + "," + y] = 0;
+  memory[x + "," + y]++;
+}
+
+console.log(Object.keys(memory).length);
