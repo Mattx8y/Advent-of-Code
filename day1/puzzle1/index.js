9707e9b0bc37562da9367503fbfba62e128e8dfa
iWillBanU <wrscr.surpertop@gmail.com>
2020-12-01 10:48:16 -0500

    Update index.js

--- day1/puzzle1/index.js
+++ day1/puzzle1/index.js
@@ -1,18 +1,14 @@
 const fs = require("fs");
-const input = fs.readFileSync("../input.txt", {encoding: "utf-8"});
+const input = fs.readFileSync("./input.txt", {encoding: "utf-8"});
 
-const inputArray = input.split("\n").map(n => parseInt(n));
-let numberA, numberB;
-(() => {
-  for (let i = 0; i < inputArray.length; i++) {
-    for (let j = i + 1; j < inputArray.length; j++) {
-      if (inputArray[i] + inputArray[j] === 2020) {
-        numberA = inputArray[i];
-        numberB = inputArray[j];
-        return;
-      }
-    }
+const inputArray = input.split("");
+let floor = 0;
+inputArray.forEach(item => {
+  if (item === "(") {
+    floor++;
+  } else if (item === ")") {
+    floor--;
   }
-})();
+});
 
-console.log(numberA * numberB);
+console.log(floor);
