3d37d531fd3668a21b302a33c7834495e4844b14
iWillBanU <wrscr.surpertop@gmail.com>
2020-12-05 01:12:20 -0500

    Create index.js

---
+++ day5/puzzle2/index.js
@@ -0,0 +1,36 @@
+const fs = require("fs");
+
+const input = fs.readFileSync("../input.txt", {encoding: "utf-8"}).split("\n");
+
+let ids = [];
+for (let i = 0; i < input.length; i++) {
+  const rows = input[i].substring(0,7).split("");
+  const columns = input[i].substring(7,10).split("");
+  
+  let rowMin = 0;
+  let rowMax = 127;
+  for (let j = 0; j < rows.length; j++) {
+    let mid = (rowMin + rowMax) / 2;
+    if (rows[j] === "F") {
+      rowMax = Math.floor(mid);
+    } else {
+      rowMin = Math.ceil(mid);
+    }
+  }
+
+  let columnMin = 0;
+  let columnMax = 7;
+  for (let j = 0; j < columns.length; j++) {
+    let mid = (columnMin + columnMax) / 2;
+    if (columns[j] === "L") {
+      columnMax = Math.floor(mid);
+    } else {
+      columnMin = Math.ceil(mid);
+    }
+  }
+
+  const id = (rowMin * 8) + columnMin;
+  ids.push(id);
+}
+
+console.log(ids.find(id => !ids.includes(id + 1) && ids.includes(id + 2)) + 1);
